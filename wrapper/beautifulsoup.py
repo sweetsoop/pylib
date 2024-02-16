a12e533bd19abae438c0580417ff2d7693f4c08d
sweetsoop <sweetsoop@gmail.com>
2024-02-16 15:23:27 +0900

    bs4 메소드 추가

--- wrapper/beautifulsoup.py
+++ wrapper/beautifulsoup.py
@@ -18,8 +18,16 @@ class BS:
         return int(re.sub(r'[^0-9]', '', BS.text(source)))
     
     @staticmethod
-    def removeNumber(source:str|Tag) -> str:
+    def remove_number(source:str|Tag) -> str:
         return BS.clean_text(re.sub(r'\d+', '', BS.text(source)))
+    
+    @staticmethod
+    def remove_element(source:list, removeElement:str) -> list:
+        return list(filter(lambda str: str != removeElement, source))
+    
+    @staticmethod
+    def single_quote_list(source:str) -> list:
+        return re.compile("(?<=')[^']+(?=')").findall(source)
     
     @staticmethod
     def clean_text(source:str|Tag) -> str:
@@ -27,14 +35,33 @@ class BS:
         return source.strip().replace('  ', ' ').removeprefix('[').removesuffix(']')
     
     @staticmethod
+    def clean_texts(source:list) -> None:
+        for x in range(len(source)):
+            source[x] = BS.clean_text(source[x])
+    
+    @staticmethod
     def texts(source:str|Tag, separate:str|None = None) -> list:
-        source = source.text if type(source) is Tag else source
+        source = BS.text(source)
         if separate is not None:
             elements = source.split(separate)
-        for x in range(len(elements)):
-            elements[x] = BS.clean_text(elements[x])
+        BS.clean_texts(elements)
         return BS.arrange_list(elements)
+    
+    @staticmethod
+    def numbers(source:str|Tag, separate:str|None = None) -> list:
+        source = BS.text(source)
+        if separate is not None:
+            elements = source.split(separate)
+        BS.clean_texts(elements)
+        newList = BS.arrange_list(elements)
+        for x in range(len(newList)):
+            newList[x] = BS.int(newList[x])
+        return newList
         
     @staticmethod
-    def arrange_list(source:list) -> list:
-        return list(filter(None, source))
+    def arrange_list(source:list, removeTag:bool = False) -> list:
+        newList = list(filter(None, source))
+        if removeTag:
+            newList = list(filter(lambda str: type(str) is not Tag, newList))
+            BS.clean_texts(newList)
+        return newList
