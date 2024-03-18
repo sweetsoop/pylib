a25add5661a8bc74030b892c02115782d4a1881f
sweetsoop <sweetsoop@gmail.com>
2024-03-18 17:28:18 +0900

    퍼센트 처리

--- util/number_util.py
+++ util/number_util.py
@@ -1,2 +1,9 @@
+import re
+
+
 class NumberUtil:
-    pass
+    @staticmethod
+    def find_percent(source:str, fallbackValue:int = 0) -> int:
+        find = re.search(r'\d+%', source)
+        return int(find.group().replace('%','')) if find is not None else fallbackValue
+        
