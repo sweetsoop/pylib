7a82b977a9cee553d46993df0de7fcd47aaafdd9
sweetsoop <sweetsoop@gmail.com>
2024-03-28 21:12:17 +0900

    today range 구하기

--- util/datetime_util.py
+++ util/datetime_util.py
@@ -6,3 +6,7 @@ class DateTimeUtil:
     def to_date(source:str, isLast:bool = False) -> datetime:
         ret = datetime.strptime(source, '%Y-%m-%d')
         return ret if isLast == False else datetime(ret.year, ret.month, ret.day, 23, 59, 59)
+    
+    def today_range() -> tuple[datetime, datetime]:
+        today = datetime.today()
+        return (datetime(today.year, today.month, today.day), datetime(today.year, today.month, today.day, 23, 59, 59))
