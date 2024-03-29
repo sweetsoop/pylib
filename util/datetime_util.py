76a184476c98090318878856ff94110a7ff28f21
sweetsoop <sweetsoop@gmail.com>
2024-03-29 15:59:23 +0900

    - 날짜 범위 - 제곱미터 찾기

--- util/datetime_util.py
+++ util/datetime_util.py
@@ -7,6 +7,5 @@ class DateTimeUtil:
         ret = datetime.strptime(source, '%Y-%m-%d')
         return ret if isLast == False else datetime(ret.year, ret.month, ret.day, 23, 59, 59)
     
-    def today_range() -> tuple[datetime, datetime]:
-        today = datetime.today()
-        return (datetime(today.year, today.month, today.day), datetime(today.year, today.month, today.day, 23, 59, 59))
+    def range_datetime(base:datetime) -> tuple[datetime, datetime]:
+        return (datetime(base.year, base.month, base.day), datetime(base.year, base.month, base.day, 23, 59, 59))
--- util/number_util.py
+++ util/number_util.py
@@ -6,4 +6,9 @@ class NumberUtil:
     def find_percent(source:str, fallbackValue:int = 0) -> int:
         find = re.search(r'\d+%', source)
         return int(find.group().replace('%','')) if find is not None else fallbackValue
+    
+    @staticmethod
+    def find_area(source:str) -> float:
+        search = re.search(r'\d{0,}.\d+\s{0,1}(?=㎡)', source)
+        return (float(search.group().replace(',','')), 0)[0]
         
